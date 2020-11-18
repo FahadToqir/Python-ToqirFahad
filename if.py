d76214062c98c227ddb74be918b801df27241dc0
FahadToqir <71391999+FahadToqir@users.noreply.github.com>
2020-11-18 19:01:43 +0100

    Add files via upload

---
+++ if.py
@@ -0,0 +1,6 @@
+﻿enter=int(input("pon un numero entero:"))
+enter2=int(input("pon otro numero entero"))
+if enter >= enter2:
+    print(enter,"es mayor o igual que",enter2)
+else:
+    print(enter,"es menor que",enter2)
