cb17a9c81e3f334798ef8f390fa2f7cf07a3ef42
FahadToqir <71391999+FahadToqir@users.noreply.github.com>
2020-11-25 18:52:49 +0100

    Add files via upload

---
+++ numeros flotantes.py
@@ -0,0 +1,8 @@
+﻿numero1=float(input("pon un numero flotante"))
+numero2=float(input("pon otro numero flotante"))
+if numero1 > numero2:
+    print(numero1,"es mayor que",numero2)
+elif numero1==numero2:
+    print(numero1,"es igual que",numero2)
+else:
+    print(numero1,"es menor que",numero2)
